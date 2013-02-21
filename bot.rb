57599545e85901fb6ec5b9a60103fc49425680ec
Abishek Bhat <bhat.abishek@gmail.com>
2013-02-21 11:22:01 +0530

    Added a Word class and indexing using a cumulative score from the probability distribution

--- bot.rb
+++ bot.rb
@@ -1,67 +1,13 @@
 $alphabets=('A'..'Z').to_a
 $indexes=(0..48).to_a
-=begin
-class Array
-	def collect_not_null
-		collection_set=[]
-			self.each_with_index do |sub_array,index|
-					collection_set << {:row=>index,:count=>7-self.count(nil)}
-				end 
-		 collection_set
-	end
-
-
-end	
-=end
-class SuffixArray
-  def initialize(the_string)
-    @the_string = the_string
-    @suffix_array = Array.new
-    last_index = the_string.length-1
-    (0..last_index).each do |i|
-      the_suffix = the_string[i..last_index]
-      the_position = i
-      @suffix_array << { :suffix=>the_suffix, :position=>the_position }
-    end
-    @suffix_array.sort! { |a,b| a[:suffix] <=> b[:suffix] }
-  end
-    
-  def find_substring(the_substring)
-    high = @suffix_array.length - 1
-    low = 0
-    while(low <= high)
-      mid = (high + low) / 2
-      this_suffix = @suffix_array[mid][:suffix]
-      compare_len = the_substring.length-1
-      comparison = this_suffix[0..compare_len]
-      if   comparison > the_substring
-        high = mid - 1
-      elsif comparison < the_substring
-        low = mid + 1
-      else 
-        return @suffix_array[mid][:position]
-      end
-    end
-    return nil
-  end
-  def print_suffix_array
-	  print @suffix_array
-  end 
-end
 
 class Bot
-	attr_accessor :input,:probability_table,:my_board,:opponent_board,:current_position,:suffix_list
+	attr_accessor :input,:my_board,:opponent_board,:current_position
 
   def initialize
-	probability=IO.read("probabilities.txt")
 	@probability_table={}
 	@my_board=Array.new(7){Array.new(7)}
 	#@opponent_board[3][3],@current_position=1,24
-	probability.split("\n").each do |line|
-		line_split=line.split(",")
-		@probability_table[line_split.first]=line_split[1]
-	end 
-	@suffix_list=IO.read("words.dat").split("\n").collect{|word| SuffixArray.new word.strip}    
   end
 
   def get_input
@@ -81,18 +27,15 @@ class Bot
   def update_board(letter,position,board)
 	board[position/7][position%7]=letter
   end
-=begin
-  def filled_indexes
-	  @my_board.collect_not_null
+
+  def fill_board
   end 
-=end 
 
   def calculate_letter(position)
     #$alphabets.sample 
 	#sorted_list=filled_indexes.sort_by{|i| i[:count]}
 	 i,j=position/7,position%7
 	 sub_string=@my_board[i].join
-	 possible_values=@suffix_list.collect{|ele| ele.find_substring(sub_string)}
   def calculate_position
 	# i,j=@current_position/7,@current_position%7
 	# if  i==7 and j==7
---
+++ length.rb
@@ -0,0 +1,34 @@
+probability_table={}
+probability=IO.read("probabilities.txt")
+probability.split("\n").each do |line|
+		line_split=line.split(",")
+		probability_table[line_split.first]=line_split[1]
+	end 
+class Word
+	attr_accessor :word,:first_letter,:middle_letter,:score,:length
+
+	def initialize(word)
+		@word=word
+		word_split=@word.split(//)
+		@length,@first_letter,@middle_letter=word.length,word_split.first,word_split[word_split.length/2]	
+	end
+end 
+
+
+words=IO.read("words.dat").split("\n").select{|i| i.strip.length == 7 or i.strip.length == 4 or i.strip.length == 3}
+word_objects=words.collect do |word|
+	_word=Word.new word.strip
+	_word.score=0.0
+	word.split(//).each{|letter| _word.score+=probability_table[letter].to_f}
+	_word
+end 
+word_objects.sort_by!{|word| word.score}.reverse!
+def mid_row(letter,word_objects)
+	word_objects.select{|word| word.middle_letter==letter}.first.word
+end 
+
+print mid_row("A",word_objects)
+
+
+
+
